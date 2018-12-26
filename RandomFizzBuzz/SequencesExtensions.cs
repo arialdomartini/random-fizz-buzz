6b24f668faa0a316330656647f83ca70dee20a8d
Arialdo Martini <arialdomartini@gmail.com>
2018-12-26 23:22:07 +0100

    Match count is generic

--- RandomFizzBuzz/SequencesExtensions.cs
+++ RandomFizzBuzz/SequencesExtensions.cs
@@ -1,12 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace RandomFizzBuzz
 {
     public static class SequencesExtensions
     {
-        public static int MatchesUpToWith(this IEnumerable<int> sequence, IEnumerable<int> reference) =>
-            sequence.TakeWhile((item, index) => item == reference.ElementAt(index)).Count();
+        public static int MatchesUpToWith<T>(this IEnumerable<T> sequence, IEnumerable<T> reference) 
+            where T : IEquatable<T> =>
+            sequence.TakeWhile((item, index) => item.Equals(reference.ElementAt(index))).Count();
 
         public static Dictionary<int, int> Classify(this Dictionary<int, List<int>> collections, IEnumerable<int> reference)
         {
